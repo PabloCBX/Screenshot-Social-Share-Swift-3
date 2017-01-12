25c7f1473c5b976c238f9f1ebcac7038db31c6dc
PabloCBX <pablocist@outlook.com>
2017-01-12 13:58:00 -0300

    Update ScreenShotAndShare.swift

--- ScreenShotAndShare.swift
+++ ScreenShotAndShare.swift
@@ -1,1 +1,15 @@
+func shareScreenshot(){
+        
+        UIGraphicsBeginImageContext(view!.frame.size)
+        view!.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let imageCap = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        UIImageWriteToSavedPhotosAlbum(imageCap!, nil, nil, nil)
+        
+        let shareImg: UIActivityViewController = UIActivityViewController(activityItems: [(imageCap!)], applicationActivities: nil)
+        self.present(shareImg, animated: true, completion: nil)
+
+    }
+    
 
